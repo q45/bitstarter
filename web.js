c091ecb6dc1858e759b8d3bb40868aec2d05782d
q45 <quintinsmith@icloud.com>
2013-07-22 19:27:24 +0000

    add readFileSync to web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Kickstarter from scratch');
+	fs.readFileSync('index.html', function(err, data) {
+	if(err) throw err;
+ 	console.log(data);
+});
+ // response.send('Kickstarter from scratch');
 });
 
 var port = process.env.PORT || 5000;
